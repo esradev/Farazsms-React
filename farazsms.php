555f4912b1bae97be3edca1b11ce4dae9662f9a9
Wpstorm <wpstormdev@gmail.com>
2023-02-11 02:45:16 +0330

    fix: Declared clalback functions as methods.

--- farazsms.php
+++ farazsms.php
@@ -12,10 +12,10 @@
  * Author URI: https://farazsms.com/
  * Text Domain: farazsms
  * Domain Path: /languages
- * 
+ *
  * WC requires at least: 3.0
  * WC tested up to: 7.1
- * 
+ *
  * @package Farazsms
  */
 
@@ -70,8 +70,8 @@ class Farazsms {
 		$this->define_constants();
 		$this->farazsms_loader();
 
-		register_activation_hook(__FILE__, 'activate_farazsms');
-		add_action('activated_plugin', 'farazsms_activation_redirect');
+		register_activation_hook( __FILE__, [ $this, 'activate_farazsms' ] );
+		add_action( 'activated_plugin', [ $this, 'farazsms_activation_redirect' ] );
 	}
 
 	/**
@@ -86,19 +86,19 @@ class Farazsms {
 		 *
 		 * @since 2.0.0
 		 */
-		define('FARAZSMS_VERSION',               '2.0.0');
-		define('FARAZSMS_FILE',                  __FILE__);
-		define('FARAZSMS_PATH',                  plugin_dir_path(FARAZSMS_FILE));
-		define('FARAZSMS_BASE',                  plugin_basename(FARAZSMS_FILE));
-		define('FARAZSMS_SLUG',                  'farazsms_settings');
-		define('FARAZSMS_SETTINGS_LINK',         admin_url('admin.php?page=' . FARAZSMS_SLUG));
-		define('FARAZSMS_CLASSES_PATH',          FARAZSMS_PATH . 'classes/');
-		define('FARAZSMS_MODULES_PATH',          FARAZSMS_PATH . 'modules/');
-		define('FARAZSMS_URL',                   plugins_url('/', FARAZSMS_FILE));
-		define('FARAZSMS_CLASSES_URL',           FARAZSMS_URL . 'classes/');
-		define('FARAZSMS_ADMIN_URL',             FARAZSMS_CLASSES_URL . 'admin/');
-		define('FARAZSMS_WEB_MAIN',              'https://farazsms.com/');
-		define('FARAZSMS_WEB_MAIN_DOC',          FARAZSMS_WEB_MAIN . 'farazsms-wordpress-plugin/');
+		define( 'FARAZSMS_VERSION', '2.0.0' );
+		define( 'FARAZSMS_FILE', __FILE__ );
+		define( 'FARAZSMS_PATH', plugin_dir_path( FARAZSMS_FILE ) );
+		define( 'FARAZSMS_BASE', plugin_basename( FARAZSMS_FILE ) );
+		define( 'FARAZSMS_SLUG', 'farazsms_settings' );
+		define( 'FARAZSMS_SETTINGS_LINK', admin_url( 'admin.php?page=' . FARAZSMS_SLUG ) );
+		define( 'FARAZSMS_CLASSES_PATH', FARAZSMS_PATH . 'classes/' );
+		define( 'FARAZSMS_MODULES_PATH', FARAZSMS_PATH . 'modules/' );
+		define( 'FARAZSMS_URL', plugins_url( '/', FARAZSMS_FILE ) );
+		define( 'FARAZSMS_CLASSES_URL', FARAZSMS_URL . 'classes/' );
+		define( 'FARAZSMS_ADMIN_URL', FARAZSMS_CLASSES_URL . 'admin/' );
+		define( 'FARAZSMS_WEB_MAIN', 'https://farazsms.com/' );
+		define( 'FARAZSMS_WEB_MAIN_DOC', FARAZSMS_WEB_MAIN . 'farazsms-wordpress-plugin/' );
 	}
 
 	public function farazsms_loader() {
@@ -115,8 +115,7 @@ class Farazsms {
 	 *
 	 * @return void
 	 */
-	public function activate_farazsms()
-	{
+	public function activate_farazsms() {
 		require_once FARAZSMS_CLASSES_PATH . 'class-farazsms-activator.php';
 		Farazsms_Activator::activate();
 	}
@@ -129,11 +128,10 @@ class Farazsms {
 	 *
 	 * @return void
 	 */
-	public function farazsms_activation_redirect($plugin)
-	{
-		if (get_option('farazsms_do_activation_redirect', false)) {
-			delete_option('farazsms_do_activation_redirect');
-			exit(wp_redirect(FARAZSMS_SETTINGS_LINK));
+	public function farazsms_activation_redirect( $plugin ) {
+		if ( get_option( 'farazsms_do_activation_redirect', false ) ) {
+			delete_option( 'farazsms_do_activation_redirect' );
+			exit( wp_redirect( FARAZSMS_SETTINGS_LINK ) );
 		}
 	}
 
