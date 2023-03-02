5b9361f125c6ed040150aa05bc407bf6ccf5f150
Wpstorm <wpstormdev@gmail.com>
2023-03-02 10:57:00 +0330

    feat: Added new table in DB for Gravity Forms.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '121cf4a0a68c02e01ae6');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '7118567c547fce25f187');
--- classes/class-farazsms-activator.php
+++ classes/class-farazsms-activator.php
@@ -10,25 +10,35 @@ class Farazsms_Activator {
 
 	public static function activate() {
 		global $wpdb;
-		$collate                = $wpdb->get_charset_collate();
-		$newsletter_table_name  = $wpdb->prefix . 'farazsms_newsletter';
-		$verify_code_table_name = $wpdb->prefix . 'farazsms_vcode';
-		$query                  = "CREATE TABLE IF NOT EXISTS `$newsletter_table_name` (
+		$collate                  = $wpdb->get_charset_collate();
+		$newsletter_table_name    = $wpdb->prefix . 'farazsms_newsletter';
+		$verify_code_table_name   = $wpdb->prefix . 'farazsms_vcode';
+		$gravity_forms_table_name = $wpdb->prefix . 'farazsms_gravity_forms';
+		$newsletter               = "CREATE TABLE IF NOT EXISTS `$newsletter_table_name` (
 					 `id` int(10) NOT NULL AUTO_INCREMENT,
 					 `phone` BIGINT(10) UNSIGNED NOT NULL ,
 					 `name` tinytext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
 					 `phone_book` int(10) DEFAULT NULL,
 					 PRIMARY KEY (`id`)
 					) $collate";
-		$query2                 = "CREATE TABLE IF NOT EXISTS `$verify_code_table_name` (
+		$verify_code              = "CREATE TABLE IF NOT EXISTS `$verify_code_table_name` (
 					 `id` int(10) NOT NULL AUTO_INCREMENT,
 					 `phone` BIGINT(10) UNSIGNED NOT NULL ,
 					 `code` int(4) DEFAULT NULL,
 					 PRIMARY KEY (`id`)
 					) $collate";
+		$gravity_forms            = "CREATE TABLE IF NOT EXISTS `$gravity_forms_table_name` (
+					 `id` int(10) NOT NULL AUTO_INCREMENT,
+					 `phonebook_id` int(10) DEFAULT NULL,
+					 `form_id` int(10) DEFAULT NULL,
+					 `field_id` int(10) DEFAULT NULL,
+					 `action` tinytext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+					 PRIMARY KEY (`id`)
+					) $collate";
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $query );
-		dbDelta( $query2 );
+		dbDelta( $newsletter );
+		dbDelta( $verify_code );
+		dbDelta( $gravity_forms );
 
 		// This option added for redirect after activation
 		add_option( 'farazsms_do_activation_redirect', true );
