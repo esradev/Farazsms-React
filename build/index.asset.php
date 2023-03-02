f1e565b017ba9487086b840b31bda1f916af11e5
Wpstorm <wpstormdev@gmail.com>
2023-03-02 10:42:31 +0330

    fix: Some bugs fixed.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'fc1b046eca438f7c7fff');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '121cf4a0a68c02e01ae6');
