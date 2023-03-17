f94757a4eeca09303af74fe027e1dbff09bef9c9
Wpstorm <wpstormdev@gmail.com>
2023-03-18 01:54:40 +0330

    fix: Fixed a bug.

--- classes/class-farazsms-ippanel.php
+++ classes/class-farazsms-ippanel.php
@@ -292,11 +292,11 @@ class Farazsms_Ippanel {
 		if ( is_wp_error( $response ) ) {
 			return false;
 		}
+
+		$response = json_decode( $response['body'], true );
 		if (!is_array($response) || !isset($response[1])) {
 			return false;
 		}
-		$response = json_decode( $response['body'], true );
-
 		$separator = '.';
 		if ( strpos( $response[1], '/' ) ) {
 			$separator = '/';
