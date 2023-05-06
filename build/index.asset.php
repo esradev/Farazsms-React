5f1c3f2d88319fa3f89eabcbbfd9f53b0604bc53
Wpstorm <wpstormdev@gmail.com>
2023-05-06 23:51:00 +0330

    fix: Fixed send sms to admin on user login based on selected roles and make build file.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'faf58a265deee6424e5f');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '87c4f6b15faad5980c0c');
